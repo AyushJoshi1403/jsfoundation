783073a242fac4a203961f996354d2f5ce245833
AyushJoshi1403 <ayush.aj1403@gmail.com>
2025-11-01 14:44:56 +0530

    practise js

---
+++ part1/logical.js
@@ -0,0 +1,14 @@
+// &&  ->  and
+// ||  ->  or
+// !   ->  reverses boolean value
+
+let isLoggedIn = true;
+let isPaidUser = false;
+
+console.log(isLoggedIn && isPaidUser);
+
+
+let isEmailuser = true;
+let isGoogleuser = false;
+
+console.log(isEmailuser || isGoogleuser);
