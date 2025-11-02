5a8095c7653258ee09f6e59401f5927f7537938d
AyushJoshi1403 <ayush.aj1403@gmail.com>
2025-11-02 23:12:25 +0530

    practise js

---
+++ part2/primitives.js
@@ -0,0 +1,32 @@
+// Number
+
+let balance = 120;
+let anotherBalance = new Number(120);
+
+// console.log(balance);
+// console.log(anotherBalance);
+// console.log(anotherBalance.valueOf());
+
+// console.log(typeof (balance));
+// console.log(typeof (anotherBalance));
+
+
+//  boolean
+let isActive = true;
+let isReallyActive = new Boolean(true);          // not recommended
+
+
+// null and undefined
+
+let firstName = null;                            // null means empty value
+let lastName;                                    // undefined means value not assigned yet
+
+// console.log(firstName);
+// console.log(lastName);
+
+
+// string
+
+let myString = "hello";
+let myStringOne = 'Hola';
+let username = 'ayush'
