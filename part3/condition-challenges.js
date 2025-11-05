d6ce082fe04d65581c4232b64e129ce80eb307b3
AyushJoshi1403 <ayush.aj1403@gmail.com>
2025-11-05 14:27:03 +0530

    practise js

--- part3/condition-challenges.js
+++ part3/condition-challenges.js
@@ -2,9 +2,67 @@
 
 let num1 = 5;
 let num2 = 8;
+console.log("I am regular upper code");
 
 if (num1 > num2) {
-  console.log(`${num1} is greater than ${num2}`);
+  console.log("num1 is greater than num2");
 } else {
-  console.log(`${num1} is not greater than ${num2}`);
+  console.log("Nope, num1 is NOT greater");
+}
+
+console.log("I am regular bottom code");
+
+console.log("-----");
+
+
+// Checking if a string is equal to another string:
+
+let username = "chai";
+let anotherUsername = "chai";
+
+if (username == anotherUsername) {
+  console.log("Pick another username");
+} else {
+  console.log("You can pick this username");
+}
+
+console.log("-----");
+
+
+// Checking if a variable is a number or not:
+
+let score = 44;
+
+if (typeof score === "number") {
+  console.log("Yep, this is a number");
+} else {
+  console.log("No that is NOT a number");
+}
+
+console.log("-----");
+
+
+// Checking if a boolean value is true or false:
+
+let isTeaReady = false;
+
+if (isTeaReady) {
+  console.log("Value is true");
+} else {
+  console.log("Value is false");
+}
+
+console.log("-----");
+
+
+// Checking array is empty or not:
+
+let items = ["item1"];
+
+console.log(items.length);
+
+if (items.length === 0) {
+  console.log("Array is empty");
+}else {
+  console.log("Array is NOT empty");
 }
